4fbf9273f7e49001f4b9a7a9bc6e017c3b513d2c
VELEZ Noellie <VELEZ.Noellie@formation.enjmin.fr>
2016-12-01 19:57:03 +0100

    Added UE4.13 project

---
+++ Project/Source/BatteryCollector/SpawnVolume.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+	virtual void BeginPlay() override;
+	
+	virtual void Tick( float DeltaSeconds ) override;
+
+	FORCEINLINE class UBoxComponent* GetWhereToSpawn() const { return WhereToSpawn; };
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* WhereToSpawn;
+
+	
+	
+};
